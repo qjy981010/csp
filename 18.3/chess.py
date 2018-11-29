16d40fa82f39d4bb92b6645975ce70827190f662
qjy981010 <1114943038@qq.com>
2018-11-29 09:20:12 +0800

    chess 85

--- 18.3/chess.py
+++ 18.3/chess.py
@@ -1,18 +1,35 @@
+# if c++: score = 100
+# elif python: score = 85
+
 N = int(input())
 
 s = []
-s.extend([slice(x*3, x*3+3) for x in range(3)])
+s.extend([slice(x * 3, x * 3 + 3) for x in range(3)])
 s.extend([slice(x, None, 3) for x in range(3)])
 s.append(slice(0, None, 4))
 s.append(slice(2, 8, 2))
 
-def process(k):
-    for i in range(9):
-        if matrix[i] == 0:
-
-
 result = []
 matrix = None
+
+
+def process(k):
+    zero_count = matrix.count(0)
+    if zero_count == 0:
+        return 0
+    finish = [x[0] for x in [matrix[j] for j in s] if x[0] == 3 - k and x.count(x[0]) == len(x)]
+    if finish:
+        return zero_count + 1 if finish[0] == 1 else -zero_count - 1
+    max_or_min = max if k==1 else min
+    max_or_min_value = -10 if k==1 else 10
+    for i in range(9):
+        if matrix[i] == 0:
+            matrix[i] = k
+            max_or_min_value = max_or_min(process(3 - k), max_or_min_value)
+            matrix[i] = 0
+    return max_or_min_value
+
+
 for i in range(N):
     matrix = []
     for j in range(3):
