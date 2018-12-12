f9e33ee413410359be92f570840287542971a23f
qjy981010 <1114943038@qq.com>
2018-12-12 19:05:02 +0800

    17.12 4 wrong but 80

--- 17.12/drive.cpp
+++ 17.12/drive.cpp
@@ -1,25 +1,72 @@
 #include <iostream>
 #include <vector>
-#include <set>
+#include <cmath>
 
 using namespace std;
 
-long n;
-long m;
-vector<vector<vector<long>>> v;
-vector<long> pre_narrow;
-vector<long> dist;
+struct Path {
+    int target;
+    int islane;
+    int length;
+    Path(int ab, int t, int c): target(ab), islane(t), length(c){}
+};
+
+struct Point {
+    int flag = 0;
+    int pre_lane = 0;
+    int dist = 1000000;
+};
+
+int n;
+int m;
+vector<Path> v[500];
+Point points[500];
+
+void dj() {
+    int source = 0;
+    while (1) {
+        int i;
+        points[source].flag = 1;
+        for (i = 0; i < v[source].size(); ++i) {
+            if (points[v[source][i].target].flag == 1) continue;
+            if (v[source][i].islane == 1) {
+                int new_dist = points[source].dist + pow(v[source][i].length + points[source].pre_lane, 2) - pow(points[source].pre_lane, 2);
+                if (new_dist < points[v[source][i].target].dist) {
+                    points[v[source][i].target].dist = new_dist;
+                    points[v[source][i].target].pre_lane = v[source][i].length + points[source].pre_lane;
+                }
+            }
+            else {
+                int new_dist = points[source].dist + v[source][i].length;
+                if (new_dist <= points[v[source][i].target].dist) {
+                    points[v[source][i].target].dist = new_dist;
+                    points[v[source][i].target].pre_lane = 0;
+                }
+            }
+        }
+        source = 0;
+        int min_dist = 1000001;
+        for (i = 0; i < n; ++i) {
+            if (points[i].flag == 1) continue;
+            else if (min_dist > points[i].dist) {
+                source = i;
+                min_dist = points[i].dist;
+            }
+        }
+        if (source == 0) break;
+    }
+}
 
 int main() {
     cin >> n >> m;
-    for (int i = 0; i <= n; ++i) {
-        v.push_back(vector<vector<long>>());
-    }
-    long t, a, b, c;
+    int t, a, b, c;
     for (int i = 0; i < m; ++i) {
         cin >> t >> a >> b >> c;
-        v[a].push_back(vector<long>({b, t, c}));
-        v[b].push_back(vector<long>({b, t, c}));
+        v[a-1].push_back(Path(b-1, t, c));
+        v[b-1].push_back(Path(a-1, t, c));
     }
+    points[0].dist = 0;
+    dj();
+    cout << points[n-1].dist << endl;
     return 0;
 }
