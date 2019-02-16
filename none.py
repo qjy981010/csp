cd88655de70379030560f9f7ef383d5371b256f7
Q . J . Y <1114943038@qq.com>
2019-02-16 20:26:18 +0800

    none

--- none.py
+++ none.py
@@ -1,6 +1,7 @@
 class Archmage(MinionCard ) :
     def __init__ (self) :
-        super().__init__("Archmage", 6, CHARACTER_CLASS.ALL, CARD_RARITY.COMMON)
+        super().__init__("Archmage", 6, CHARACTER_CLASS.ALL,
+                         CARD_RARITY.COMMON)
 
     def create_minion (self, player) :
         return Minion(4, 7, spell_damage = 1)
